dd12f459cb11061eebb073d5f8235236c62833d0
Bharadwaj <rbharadwaj022@gmail.com>
2024-11-16 14:51:30 +0530

    Changes

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "tailwindcss/tailwind.css";
 import Registered from "./Registered";
 
 function Courses({ user }) {
+  const [enrollments, setEnrollments] = useState({});
+  const [view, setView] = useState(false);
+
+  const fetchEnrollments = async () => {
+    const response = await fetch("http://127.0.0.1:5500/get_courses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: user.id }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setEnrollments(data);
+      console.log(data);
+    } else {
+      alert("Failed to fetch enrollments");
+    }
+  };
+
   const handleEnroll = async (courseId) => {
     const response = await fetch(
       `http://127.0.0.1:5500/register_course/${courseId}`,
@@ -17,16 +38,19 @@ function Courses({ user }) {
 
     if (response.ok) {
       alert("Successfully registered");
+      fetchEnrollments(); // Fetch enrollments immediately after successful registration
     } else {
       alert("Registration failed");
     }
   };
 
+  useEffect(() => {
+    fetchEnrollments();
+  }, [user.id]);
+
   const handleEnrolled = async () => {
     setView(true);
   };
-
-  const [view, setView] = useState(false);
 
   return (
     <>
@@ -36,42 +60,31 @@ function Courses({ user }) {
             Available Courses
           </h1>
           <div className="flex flex-wrap justify-center gap-8">
-            <div className="bg-white p-6 rounded-2xl shadow-lg flex-1 max-w-xs text-center">
-              <h3 className="text-2xl font-bold mb-4 text-gray-800">MongoDB</h3>
-              <div className="flex justify-center mb-4">
-                <button
-                  onClick={() => handleEnroll(1)}
-                  className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg mx-2 hover:scale-105 transition-transform duration-300 ease-in-out"
+            {enrollments.courses &&
+              enrollments.courses.map((course) => (
+                <div
+                  key={course.id}
+                  className="bg-white p-6 rounded-2xl shadow-lg flex-1 max-w-xs text-center"
                 >
-                  Enroll Now
-                </button>
-              </div>
-            </div>
-            <div className="bg-white p-6 rounded-2xl shadow-lg flex-1 max-w-xs text-center">
-              <h3 className="text-2xl font-bold mb-4 text-gray-800">Flask</h3>
-              <div className="flex justify-center mb-4">
-                <button
-                  onClick={() => handleEnroll(2)}
-                  className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg mx-2 hover:scale-105 transition-transform duration-300 ease-in-out"
-                >
-                  Enroll Now
-                </button>
-              </div>
-            </div>
-            <div className="bg-white p-6 rounded-2xl shadow-lg flex-1 max-w-xs text-center">
-              <h3 className="text-2xl font-bold mb-4 text-gray-800">Git</h3>
-              <div className="flex justify-center mb-4">
-                <button
-                  onClick={() => handleEnroll(3)}
-                  className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg mx-2 hover:scale-105 transition-transform duration-300 ease-in-out"
-                >
-                  Enroll Now
-                </button>
-              </div>
-            </div>
+                  <h3 className="text-2xl font-bold mb-4 text-gray-800">
+                    {course.name === "R Software" ? "Git" : course.name}
+                  </h3>
+                  <p className="text-gray-600 mb-4 font-bold">
+                    Enrollments: {course.no_of_enrollments}
+                  </p>
+                  <div className="flex justify-center mb-4">
+                    <button
+                      onClick={() => handleEnroll(course.id)}
+                      className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg mx-2 hover:scale-105 transition-transform duration-300 ease-in-out"
+                    >
+                      Enroll Now
+                    </button>
+                  </div>
+                </div>
+              ))}
           </div>
           <button
-            onClick={() => handleEnrolled()}
+            onClick={handleEnrolled}
             className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out shadow-lg m-4"
           >
             View Enrolled Courses
--- src/components/Registered.jsx
+++ src/components/Registered.jsx
@@ -15,7 +15,7 @@ function Registered({ user, view, setView }) {
       .then((response) => response.json())
       .then((data) => setCourses(data.registered_courses))
       .catch((error) => console.error("Error fetching courses:", error));
-  }, [user.id]);
+  }, [user.id, courses]);
 
   const getCourseName = (courseId) => {
     switch (courseId) {
@@ -50,6 +50,31 @@ function Registered({ user, view, setView }) {
   const [assignment, setAssignment] = useState(null);
   const [viewAssignments, setViewAssignments] = useState(false);
 
+  const handleUnenroll = (courseId) => {
+    fetch(`http://127.0.0.1:5500/delete_registration/${courseId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: user.id }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        // Refetch courses after unenrollment
+        fetch("http://127.0.0.1:5500/get_registered_courses", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: user.id }),
+        })
+          .then((response) => response.json())
+          .then((data) => setCourses(data.registered_courses))
+          .catch((error) => console.error("Error fetching courses:", error));
+      })
+      .catch((error) => console.error("Error unenrolling from course:", error));
+  };
+
   return (
     <>
       {!viewAssignments ? (
@@ -71,9 +96,30 @@ function Registered({ user, view, setView }) {
                   key={index}
                   className="bg-gray-100 p-6 rounded-lg shadow-md"
                 >
-                  <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                    {getCourseName(course.course_id)}
-                  </h3>
+                  <div className="flex justify-around items-center">
+                    <h3 className="text-2xl font-semibold mb-4 text-gray-700 ">
+                      {getCourseName(course.course_id)}
+                    </h3>
+                    <button
+                      onClick={() => handleUnenroll(course.course_id)}
+                      className="bg-gradient-to-r from-red-400 to-red-600 text-white px-4 py-2 rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M4 6h16M4 6v2a2 2 0 002 2h12a2 2 0 002-2V6M8 6V4a1 1 0 011-1h6a1 1 0 011 1v2M5 10h14v10a2 2 0 01-2 2H7a2 2 0 01-2-2V10z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                   <div className="flex justify-between">
                     <button
                       onClick={() => {
